a8de74128ffe1f196b61e01c88f363e73469ede7
yasinpalash <yasinmiapolash@gmail.com>
2025-03-08 18:00:54 +0600

    update .gitignore and main.dart; remove unused provider and clean up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final nameProvider = Provider<String>((ref) {
-  return "Hello Yasin";
-});
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -17,23 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
     );
@@ -49,11 +30,15 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Riverpod Provider"),
       ),
-      body: Center(
-        child: Consumer(builder: (context,ref,child){
-          final name=ref.watch(nameProvider);
-          return Text(name);
-        }),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Riverpod Provider',
+            ),
+          ],
+        ),
       ),
     );
   }
